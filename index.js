ffcba113aeb5213c86ccc1e4789d46609cbaed5c
SomeBodyM <151923587+SomeBodyM@users.noreply.github.com>
2024-08-13 21:23:50 +0300

    cors

--- index.js
+++ index.js
@@ -6,29 +6,21 @@ const cookieParser = require('cookie-parser');
 const router = require('./routes/index');
 const ErrorMiddleware = require('./middlewares/error-maddleware');
 
-const app = express()
-const PORT = process.env.PORT || 7000
+const app = express();
+const PORT = process.env.PORT || 7000;
 
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 
-app.use('/api', cors({
-        credentials: true,
-        origin: process.env.CLIENT_URL,
-        exposedHeaders: ["Set-Cookie"]
-    }
-))
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+    exposedHeaders: ["Set-Cookie"]
+}));
 
 app.set("trust proxy", 1);
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL);
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    next();
-});
-app.use('/api', router)
-app.use(ErrorMiddleware)
+app.use('/api', router);
+app.use(ErrorMiddleware);
 
 const server = async () => {
     try {
@@ -36,10 +28,10 @@ const server = async () => {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
-        app.listen(PORT, () => console.log(PORT))
+        app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
     } catch (e) {
-        console.log(e)
+        console.log(e);
     }
-}
+};
 
-server()
+server();
