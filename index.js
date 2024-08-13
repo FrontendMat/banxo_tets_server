f31f93d33dece72a950ceb1fda5fe490affa764c
SomeBodyM <151923587+SomeBodyM@users.noreply.github.com>
2024-08-13 21:13:59 +0300

    cors

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use('/api', cors({
     }
 ))
 
+app.set("trust proxy", 1);
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL);
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
